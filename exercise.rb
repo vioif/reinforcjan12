ae8d069879902969d8c6516b72260e839d3276b2
vioif <ireneaflores3@gmail.com>
2018-01-15 16:28:35 -0500

    exercise completed

--- exercise.rb
+++ exercise.rb
@@ -4,3 +4,26 @@ seating = [
   ["Gismo", "Shadow", nil, nil]
   ["Smokey", "Toast", "Pacha", "Mau"]
 ]
+
+chair = "n"
+
+seating.each_with_index do |row, row_num|
+  row.each_with_index do |seat, seat_num|
+    if !seat
+      puts "row #{row_num + 1} seat #{seatnum + 1} is free. Would you like to sit here?"
+      sit = gets.chomp
+      if sit == 'y'
+        puts "What is your name?"
+        name = gets.chomp
+        seats[row_num][seat_num] = name
+        break
+      end
+    end
+    if sit == 'y'
+      break
+    end
+  end
+  if sit == 'y'
+    break
+  end
+end
